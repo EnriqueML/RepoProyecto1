7d6898879b81de33193fc820b0cc529698251056
Enrique <enrique@hotmail.com>
2014-04-03 13:51:59 +0200

    Primera version del proyecto

---
+++ GitEntornos/src/entornos/Principal.java
@@ -0,0 +1,11 @@
+package entornos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("--FIN DE PROGRAMA--");
+
+	}
+
+}
